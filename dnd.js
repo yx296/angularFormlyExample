f544754690f459140e3d3254c9e83c44a0a4a74c
Yilin Xu <yx296@nyu.edu>
2016-03-11 10:15:48 -0500

    take out old code

--- dnd.js
+++ dnd.js
@@ -1,57 +1,44 @@
 app.directive('dndBetween', function() {
   return {
     link: function (scope, element, attrs) { 
-      var args = attrs.dndBetween.split(',');
-      
-      var listModel = args[0];
-      var otherListId = args[1];
 
-      var targetArgs = $('#' + otherListId).attr('dnd-between').split(','); 
-      //get access to other listModel through targetArgs
-
-      var otherListModel = targetArgs[0];
-
-
-      var source;
-      var target;
-      var startIndex;
-
-      scope.$watch(listModel, function(value) {
-        source = value;
-        console.log('source', source);
-      }, true)
-
-      scope.$watch(otherListModel, function(value) {
-        target = value;
-        console.log('target', target);
-      }, true)
-
-
-      $(element[0]).sortable({
-        items: 'li',
-        start: function (event, ui) { 
-          startIndex = ui.item.index();
-          console.log(startIndex);
-        },
-        stop: function(event, ui) { 
-          var newParent = ui.item[0].parentNode.id;
-          var newIndex = ui.item.index();
-
-          var toMove = source[startIndex];
-          source.splice(startIndex, 1);
-
-          if (newParent === otherListId) {
-            target.splice(newIndex, 0, toMove);
-          } else {
-            source.splice(newIndex, 0, toMove);
-          }
-
-          // scope.$apply();
-          // scope.$apply(source);
-          // scope.$apply(target);
-        },
-        connectWith: '#' + otherListId
-      })
     }
   }
 });
+
+
+app.directive('drag', ['$document', function($document) {
+  return {
+    link: function(scope, element, attr) {
+      var startX = 0, startY = 0, x = 0, y = 0;
+
+      element.css({
+       position: 'relative',
+       cursor: 'pointer'
+      });
+
+      element.on('mousedown', function(event) {
+        // Prevent default dragging of selected content
+        event.preventDefault();
+        startX = event.pageX - x;
+        startY = event.pageY - y;
+        $document.on('mousemove', mousemove);
+        $document.on('mouseup', mouseup);
+      });
+
+      function mousemove(event) {
+        y = event.pageY - startY;
+        x = event.pageX - startX;
+        element.css({
+          top: y + 'px',
+          left:  x + 'px'
+        });
+      }
+
+      function mouseup() {
+        $document.off('mousemove', mousemove);
+        $document.off('mouseup', mouseup);
+      }
+    }
+  };
+}]);
