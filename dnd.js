47d58d19c02cb843083f1cf33aee7063935ef018
Yilin Xu <yx296@nyu.edu>
2016-03-11 13:46:04 -0500

    with two Directives

--- dnd.js
+++ dnd.js
@@ -1,15 +1,37 @@
-app.directive('dragDrop', function() {
+app.directive('dropzone', function() {
   return {
     link: function (scope, element, attrs) { 
-      var onDrop = element[0].ondrop;
-      console.log(onDrop);
-      // // console.log(element);
-      // element[0].addEventListener(onDrop, function() {
-      //   console.log('wtf');
-      // })
-      // // element.on(onDrop, function() {
-      // //   console.log('jabba');
-      // // })
+      var element = element[0];
+
+      element.ondrop = drop;
+      element.ondragover = allowDrop;
+
+      function allowDrop(ev) {
+        ev.preventDefault();
+      }
+
+      function drop(ev) {
+        ev.preventDefault();
+        var data = ev.dataTransfer.getData("id");
+        ev.target.appendChild(document.getElementById(data));
+        console.log(ev.target);
+      }
+    }
+  }
+});
+
+
+app.directive('dnd', function() {
+  return {
+    link: function (scope, element, attrs) { 
+      var element = element[0];
+      
+      element.draggable = true;
+      element.ondragstart = drag;
+
+      function drag(ev) {
+        ev.dataTransfer.setData("id", ev.target.id);
+      }
     }
   }
 });
