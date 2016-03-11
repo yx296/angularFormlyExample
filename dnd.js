4067890fc560dc46bee3c7b359f77a6ebdb31ab2
Yilin Xu <yx296@nyu.edu>
2016-03-11 12:02:53 -0500

    switch to new branch

--- dnd.js
+++ dnd.js
@@ -1,7 +1,15 @@
 app.directive('dragDrop', function() {
   return {
     link: function (scope, element, attrs) { 
-      
+      var onDrop = element[0].ondrop;
+      console.log(onDrop);
+      // // console.log(element);
+      // element[0].addEventListener(onDrop, function() {
+      //   console.log('wtf');
+      // })
+      // // element.on(onDrop, function() {
+      // //   console.log('jabba');
+      // // })
     }
   }
 });
