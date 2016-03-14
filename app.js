53aad26f0976c559c31885eaa5163da8ec3c7ded
Yilin Xu <yx296@nyu.edu>
2016-03-14 01:06:57 -0400

    add save and load state

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ app.controller("testCtrl", function() {
   vm.boxOneItems = ['lunch','dinner'];
   vm.boxTwoItems = ['breakfast', '2nd breakfast'];
 
+  var savedBoxOne;
+  var savedBoxTwo;
+
   vm.addToBoxOne = function() {
     vm.boxOneItems.push(vm.toDo);
     vm.toDo = '';
@@ -20,4 +23,16 @@ app.controller("testCtrl", function() {
     if (!vm.boxTwoItems.length) { return; }
     vm.boxOneItems.unshift(vm.boxTwoItems.pop());
   }
+
+  vm.saveState = function() {
+    savedBoxOne = vm.boxOneItems.slice();
+    savedBoxTwo = vm.boxTwoItems.slice();
+  }
+
+  vm.loadState = function() {
+    vm.boxOneItems = savedBoxOne.slice();
+    vm.boxTwoItems = savedBoxTwo.slice();
+  }
+
+  vm.saveState();
 });
