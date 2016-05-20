27848e2959d2c6c7b4ada2e1d16561d01765005f
Yilin Xu <yx296@nyu.edu>
2016-05-19 22:05:11 -0400

    formly fun

--- app.js
+++ app.js
@@ -2,6 +2,22 @@
 
     'use strict';
 
-    angular.module('formlyApp', ['formly', 'formlyBootstrap']);
+    angular.module('formlyApp', ['formly', 'formlyBootstrap']).
+    config(config).
+    run(run);
+
+    function config($locationProvider, formlyConfigProvider) {
+        $locationProvider.html5Mode(true).hashPrefix('!');
+        
+        formlyConfigProvider.extras.errorExistsAndShouldBeVisibleExpression = function($viewValue, $modelValue, scope) {
+            return (scope.fc.$invalid && scope.form.$submitted);
+        };
+    }
+
+    function run(formlyValidationMessages) {
+        formlyValidationMessages.addStringMessage('required', 'This field is required');
+        console.log(formlyValidationMessages);
+    }
+
 
 })();
